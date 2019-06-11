d0e10e2551f4d265a2d04eaf47f8cc7d0d85664e
Tatyumi <s150204@nishitech.ac.jp>
2019-06-11 20:48:20 +0900

    生成処理の変更
    Generatorクラスをベースとし、各継承先のクラスで生成の制御を行うようにした。

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -11,56 +11,34 @@ public class EnemyGenerator : MonoBehaviour
     public Transform[] EnemyGeneratSpots;
     /// <summary>ポーズマネージャー</summary>
     public GameObject PauseManager;
-    /// <summary>敵キャラのプレファブ</summary>
-    public GameObject EnemyPrefab;
-    /// <summary>生成間隔</summary>
-    private float span = 1.0f;
-    /// <summary>計測時間</summary>
-    private float delta = 0.0f;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // 初期化
-        Initialize();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // 時間計測
-        delta += Time.deltaTime;
-
-        // 一定時間経過したか判別
-        if (delta > span)
-        {
-            foreach (var item in EnemyGeneratSpots)
-            {
-                //生成するプレファブをゲームオブジェクトに変換
-                var gameObject = Instantiate(EnemyPrefab) as GameObject;
-
-                //ゲームオブジェクトをPauseManagerの子にする
-                gameObject.transform.SetParent(PauseManager.transform, false);
-
-                // ゲームオブジェクトを指定の座標に配置
-                gameObject.transform.SetPositionAndRotation(item.position, item.rotation);
-            }
-
-            // 初期化
-            delta = 0.0f;
-        }
-    }
 
     /// <summary>
     /// 初期化
     /// </summary>
-    private void Initialize()
+    protected void Initialize()
     {
         // 要素数分配列数を確保
         EnemyGeneratSpots = new Transform[transform.childCount];
 
         // 子要素の生成場所を配列で取得
         EnemyGeneratSpots = gameObject.GetComponentsInChildrenWithoutSelf<Transform>();
+    }
+
+
+    /// <summary>
+    /// 指定した場所から敵キャラを生成する
+    /// </summary>
+    /// <param name="enemyPrefab">敵キャラオブジェクト</param>
+    /// <param name="spotNum">生成場所番号</param>
+    protected void GeneratEnemyObj(GameObject enemyPrefab,int spotNum)
+    {
+        //生成するプレファブをゲームオブジェクトに変換
+        var gameObject = Instantiate(enemyPrefab) as GameObject;
+
+        //ゲームオブジェクトをPauseManagerの子にする
+        gameObject.transform.SetParent(PauseManager.transform, false);
+
+        // ゲームオブジェクトを指定の座標に配置
+        gameObject.transform.SetPositionAndRotation(EnemyGeneratSpots[spotNum].position, EnemyGeneratSpots[spotNum].rotation);
     }
 }
---
+++ Assets/Scripts/FirstStageEnemyGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstStageEnemyGenerator : EnemyGenerator
+{
+    /// <summary>敵キャラのプレファブ</summary>
+    public GameObject EnemyPrefab;
+    /// <summary>生成間隔</summary>
+    private float span = 2.0f;
+    /// <summary>計測時間</summary>
+    private float delta = 0.0f;
+
+    /// <summary>生成場所番号</summary>
+    private enum GeneratSpot
+    {
+        None = -1,
+        Bottom,
+        BottomLeft,
+        TopLeft,
+        Top,
+        TopRight,
+        BottomRight
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 初期化
+        base.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 時間計測
+        delta += Time.deltaTime;
+
+        // 一定時間経過したか判別
+        if (delta > span)
+        {
+            // 生成
+            base.GeneratEnemyObj(EnemyPrefab, (int)GeneratSpot.Bottom);
+
+            // 初期化
+            delta = 0.0f;
+        }
+    }
+}

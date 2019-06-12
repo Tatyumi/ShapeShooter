d369ab589ce245f2f79b3318cdfd9f851bbf00f2
Tatyumi <s150204@nishitech.ac.jp>
2019-06-12 12:33:11 +0900

    新規追加

---
+++ Assets/Scripts/ZigzagEnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class ZigzagEnemyController : EnemyController
+{
+    // ステージの一部
+    public GameObject StagePart;
+    /// <summary>ステージの横幅</summary>
+    private float stageWidth;
+
+    private void Start()
+    {
+        // 初期化
+        base.Initialize();
+
+        // ステージの横幅を取得
+        stageWidth = StagePart.transform.localScale.x;
+    }
+
+
+    private void Update()
+    {
+        // 移動
+        Move();
+    }
+
+    /// <summary>
+    /// 左右に揺れながら移動する
+    /// </summary>
+    protected override void Move()
+    {
+        // 左右に揺れながら移動
+        transform.localPosition = new Vector3((transform.localPosition.x + Mathf.Sin(Time.time * stageWidth))/2　, transform.localPosition.y, transform.localPosition.z + moveSpeed);
+    }
+
+
+}

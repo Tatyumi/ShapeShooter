6f848fab61fe84d917c0fad59676a56c5c6df2d2
Tatyumi <s150204@nishitech.ac.jp>
2019-06-13 02:55:17 +0900

    Constansクラスの追加
    共通する定数を持つクラスを追加した。

--- Assets/Scripts/Common.cs
+++ Assets/Scripts/Common.cs
@@ -1,10 +1,18 @@
 ﻿namespace Common
 {
+    /// <summary>シーン名を持つクラス</summary>
     public sealed class SceneName
     {
         /// <summary>タイトルシーン名</summary>
         public const string TITLE_SCENE = "TitleScene";
         /// <summary>ファーストステージ名</summary>
         public const string FIRST_STAGE_SCENE = "FirstStageScene";
+    }
+
+    /// <summary>定数を持つクラス</summary>
+    public static class Constans
+    {
+        /// <summary>ファーストステージのステージ部品の横幅</summary>
+        public const float FIRST_STAGE_PART_WIDTH = 5.0f;
     }
 }

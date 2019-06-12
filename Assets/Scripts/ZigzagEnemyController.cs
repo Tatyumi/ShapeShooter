205f379e68f548339c9ffd9e048d5286114e6911
Tatyumi <s150204@nishitech.ac.jp>
2019-06-13 02:54:36 +0900

    左右に揺れながら動く処理の追加
    ステージの大きさに沿って揺れながら動く処理を追加した。のちのステージ追加により、ステージ幅の取得方法を変更する予定である。

--- Assets/Scripts/ZigzagEnemyController.cs
+++ Assets/Scripts/ZigzagEnemyController.cs
@@ -1,28 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Common;
 using UnityEngine;
 
 public sealed class ZigzagEnemyController : EnemyController
 {
-    // ステージの一部
-    public GameObject StagePart;
-    /// <summary>ステージの横幅</summary>
-    private float stageWidth;
+    /// <summary>最小x座標</summary>
+    private float minPosX;
+    /// <summary>最大x座標</summary>
+    private float maxPosX;
+    /// <summary>左右に動く速度</summary>
+    private float zigzagSpeed = 0.1f;
 
     private void Start()
     {
         // 初期化
         base.Initialize();
 
-        // ステージの横幅を取得
-        stageWidth = StagePart.transform.localScale.x;
-    }
+        // ステージの横幅(半分)を取得
+        var stageHalfWidth = Constans.FIRST_STAGE_PART_WIDTH / 2;
 
+        // 可動範囲
+        var moveRange = stageHalfWidth - transform.localScale.x;
 
-    private void Update()
-    {
-        // 移動
-        Move();
+        // ラジアン
+        var radian = transform.localRotation.z * Mathf.Deg2Rad;
+
+        // 角度を考慮した最小、最大x座標を取得
+        minPosX = transform.position.x - (moveRange * Mathf.Cos(radian));
+        maxPosX = transform.position.x + (moveRange * Mathf.Cos(radian));
     }
 
     /// <summary>
@@ -30,9 +34,14 @@ public sealed class ZigzagEnemyController : EnemyController
     /// </summary>
     protected override void Move()
     {
-        // 左右に揺れながら移動
-        transform.localPosition = new Vector3((transform.localPosition.x + Mathf.Sin(Time.time * stageWidth))/2　, transform.localPosition.y, transform.localPosition.z + moveSpeed);
+        // 移動
+        transform.Translate(zigzagSpeed, 0, moveSpeed);
+
+        // 最大または最小のx座標に達した場合
+        if (transform.localPosition.x > maxPosX || transform.localPosition.x < minPosX)
+        {
+            // 動きを反転させる
+            zigzagSpeed *= -1;
+        }
     }
-
-
 }

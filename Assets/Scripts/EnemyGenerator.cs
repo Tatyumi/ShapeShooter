6bdf9acd5d8908d43aa9f15fb65c310d8ca526c4
Tatyumi <s150204@nishitech.ac.jp>
2019-06-13 13:51:55 +0900

    コルーチンによる敵キャラ生成処理の実装
    コルーチンを用いることで規則的に敵キャラを生成する処理を実装した。

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using System.Threading;
 
-public class EnemyGenerator : MonoBehaviour
+public abstract class EnemyGenerator : MonoBehaviour
 {
-    // ステージのパーツ
+    /// <summary>ステージパーツ</summary>
     public GameObject StagePart;
     /// <summary>ポーズマネージャー</summary>
     public GameObject PauseManager;
-    // 各生成場所
+    /// <summary>経過時間</summary>
+    protected float delta;
+    /// <summary>ゲーム進捗度</summary>
+    protected int phase;
+    /// <summary>フェイズ開始時間</summary>
+    protected int phaseStartTime;
+    /// <summary>各生成場所</summary>
     private Transform[] EnemyGeneratSpots;
+    /// <summary>コルーチン</summary>
+    protected IEnumerator corutine;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         // 初期化
         Initialize();
@@ -22,21 +31,29 @@ public class EnemyGenerator : MonoBehaviour
     /// <summary>
     /// 初期化
     /// </summary>
-    protected void Initialize()
+    void Initialize()
     {
         // 要素数分配列数を確保
         EnemyGeneratSpots = new Transform[transform.childCount];
 
         // 子要素の生成場所を配列で取得
         EnemyGeneratSpots = gameObject.GetComponentsInChildrenWithoutSelf<Transform>();
+
+        // ゲーム進捗初期化
+        phase = 1;
+        phaseStartTime = 3;
     }
+
+    // 生成開始処理
+    protected abstract IEnumerator StartGenerat();
+
 
     /// <summary>
     /// 指定した場所から敵キャラを生成する
     /// </summary>
     /// <param name="enemyPrefab">敵キャラオブジェクト</param>
     /// <param name="spotNum">生成場所番号</param>
-    protected void GeneratEnemyObj(GameObject enemyPrefab,int spotNum)
+    protected void GeneratEnemyObj(GameObject enemyPrefab, int spotNum)
     {
         //生成するプレファブをゲームオブジェクトに変換
         var gameObject = Instantiate(enemyPrefab) as GameObject;
--- Assets/Scripts/FirstStageEnemyGenerator.cs
+++ Assets/Scripts/FirstStageEnemyGenerator.cs
@@ -1,15 +1,12 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public sealed class FirstStageEnemyGenerator : EnemyGenerator
 {
-    /// <summary>敵キャラのプレファブ</summary>
-    public GameObject EnemyPrefab;
-    /// <summary>生成間隔</summary>
-    private float span = 2.0f;
-    /// <summary>計測時間</summary>
-    private float delta = 0.0f;
+    /// <summary>スタンダードエネミーのプレファブ</summary>
+    public GameObject StandardEnemyPrefab;
+    /// <summary>ジグザグエネミーのプレファブ</summary>
+    public GameObject ZigzagdEnemyPrefab;
 
     /// <summary>生成場所番号</summary>
     private enum GeneratSpot
@@ -23,20 +20,160 @@ public sealed class FirstStageEnemyGenerator : EnemyGenerator
         BottomRight
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        // 時間計測
-        delta += Time.deltaTime;
+        // コルーチンの取得
+        corutine = StartGenerat();
 
-        // 一定時間経過したか判別
-        if (delta > span)
+        // 処理開始
+        StartCoroutine(corutine);
+    }
+
+    protected override IEnumerator StartGenerat()
+    {
+        // 次のフェイズまでの待機時間
+        var nextPhase = new WaitForSeconds(3.5f);
+        // 待機時間
+        var wait = new WaitForSeconds(1);
+        // 連続して生成する回数
+        var consecutivelyGeneratCount = 3;
+
+        // 待機
+        yield return nextPhase;
+
+        // 左下に３体生成
+        for (int i = 0; i < consecutivelyGeneratCount; i++)
         {
-            // 生成
-            base.GeneratEnemyObj(EnemyPrefab, (int)GeneratSpot.Bottom);
-
-            // 初期化
-            delta = 0.0f;
+            GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomLeft);
+            yield return wait;
         }
+
+        // 右下に３体生成
+        for (int i = 0; i < consecutivelyGeneratCount; i++)
+        {
+            GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomRight);
+            yield return wait;
+        }
+
+        // 待機
+        yield return nextPhase;
+
+        // 下に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Bottom);
+        yield return wait;
+
+        // 左下に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomLeft);
+        yield return wait;
+
+        // 左上に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopLeft);
+        yield return wait;
+
+        // 待機
+        yield return nextPhase;
+
+        // 上に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Top);
+        yield return wait;
+
+        // 右上に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopRight);
+        yield return wait;
+
+        // 右下に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomRight);
+        yield return wait;
+
+        // 待機
+        yield return nextPhase;
+
+        // 左上に３体生成
+        for (int i = 0; i < consecutivelyGeneratCount; i++)
+        {
+            GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopLeft);
+            yield return wait;
+        }
+
+        // 上に３体生成
+        for (int i = 0; i < consecutivelyGeneratCount; i++)
+        {
+            GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Top);
+            yield return wait;
+        }
+
+        // 待機
+        yield return nextPhase;
+
+        // 右上,右下に生成に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopRight);
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomRight);
+
+        // 待機
+        yield return nextPhase;
+
+        // 左上,左下に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopLeft);
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomLeft);
+
+        // 待機
+        yield return nextPhase;
+
+        // 上、左上、右上に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Top);
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopLeft);
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopRight);
+        yield return wait;
+
+        // 待機
+        yield return nextPhase;
+
+        // 下、左下、右下に生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Bottom);
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomLeft);
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomRight);
+        yield return wait;
+
+        // 待機
+        yield return nextPhase;
+
+        // 2週順に敵を生成
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Bottom);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomLeft);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopLeft);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Top);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopRight);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomRight);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Bottom);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomLeft);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopLeft);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Top);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.TopRight);
+        yield return wait;
+        GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.BottomRight);
+        yield return wait;
+
+        // 下に10体生成
+        for (int i = 0; i < 10; i++)
+        {
+            GeneratEnemyObj(StandardEnemyPrefab, (int)GeneratSpot.Bottom);
+            yield return wait;
+        }
+
+        // 待機
+        yield return nextPhase;
+
+        // 終了
+        yield return null;
     }
 }

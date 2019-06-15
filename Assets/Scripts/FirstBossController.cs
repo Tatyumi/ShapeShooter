186d1c55e5dac22f87f5c2ae3f15678ae68ab76a
Tatyumi <s150204@nishitech.ac.jp>
2019-06-15 16:53:08 +0900

    新規追加

---
+++ Assets/Scripts/FirstBossController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstBossController : EnemyController
+{
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    protected override void Move()
+    {
+        // 回転処理
+        transform.Rotate(moveSpeed, moveSpeed, 0);
+    }
+}

8ac0a1c0fe93abd3d01ad57781791dc793b49702
Tatyumi <s150204@nishitech.ac.jp>
2019-06-15 09:47:53 +0900

    新規作成

---
+++ Assets/Scripts/SceneMoveActionUIController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class SceneMoveActionUIController : MonoBehaviour
+{
+    /// <summary>フェードインスピード</summary>
+    protected float fadeInSpeed = 0.002f;
+    /// <summary>オーディオマネージャー</summary>
+    protected AudioManager audioManager;
+    /// <summary>目標アルファ値</summary>
+    protected float targetAlpha = 0.8f;
+    /// <summary>アクションパネル</summary>
+    private Image ActionPanel;
+    /// <summary>色(RGB)</summary>
+    private float red, green, blue;
+    /// <summary>アルファ値</summary>
+    private float alpha = 0.0f;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        // 初期化
+        Initialize();
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        // ゲームオブジェクトパネルがアクティブになった場合
+        if (gameObject.activeSelf)
+        {
+            // アルファ値を加算しフェードインする
+            ActionPanel.color = new Color(red, green, blue, alpha);
+            alpha += fadeInSpeed;
+
+            // 一定のアルファ値に達した場合
+            if (ActionPanel.color.a >= targetAlpha)
+            {
+                // シーン遷移時の演出を起こす
+                SceneMoveAction();
+            }
+        }
+    }
+
+    // 初期化処理
+    private void Initialize()
+    {
+        // imageコンポーネントの取得
+        ActionPanel = gameObject.GetComponent<Image>();
+
+        // ゲームオーバーパネルの色(RGB)の値を取得
+        red = ActionPanel.color.r;
+        green = ActionPanel.color.g;
+        blue = ActionPanel.color.b;
+
+        // 音楽データの取得
+        audioManager = AudioManager.Instance;
+
+        // ゲームオブジェクトを非表示
+        gameObject.SetActive(false);
+    }
+
+    /// <summary>シーン遷移演出</summary>
+    protected abstract void SceneMoveAction();
+}
---
+++ Assets/Scripts/WhiteLightImageController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using Common;
+
+public class WhiteLightImageController : SceneMoveActionUIController
+{
+    /// <summary>シーン遷移時の演出</summary>
+    protected override void SceneMoveAction()
+    {
+        // 音の停止
+        audioManager.StopSound();
+
+        // ボスシーンに遷移
+        SceneManager.LoadScene(SceneName.BOSS_SCENE);
+    }
+}

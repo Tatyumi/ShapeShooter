47c33b38fa73574607adea793d6e1ce1f07c62b2
Tatyumi <s150204@nishitech.ac.jp>
2019-06-16 09:42:43 +0900

    新規追加

---
+++ Assets/Scripts/BossSceneMainCameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public sealed class BossSceneMainCameraController : MainCameraController
+{
+    protected override void Move()
+    {
+        // →を押した場合
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // 対象オブジェクトの座標を中心に右に回転
+            transform.RotateAround(TargetObj.transform.position, Vector3.down, moveSpeed);
+        }
+
+        // ←を押した場合
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // 対象オブジェクトの座標を中心に左に回転
+            transform.RotateAround(TargetObj.transform.position, Vector3.up, moveSpeed);
+        }
+    }
+}

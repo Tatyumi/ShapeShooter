7ad0afa0fbf341da176af409a7a5b8f9fcd8e71d
Tatyumi <s150204@nishitech.ac.jp>
2019-06-17 16:21:16 +0900

    新規追加

---
+++ Assets/Scripts/EnemyBulletController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour
+{
+    /// <summary>弾が発する光</summary>
+    public GameObject BulletLight;
+    /// <summary>弾速</summary>
+    private float speed = 0.2f;
+    /// <summary>破棄時間</summary>
+    private float deletTime = 3.0f;
+    /// <summary>計測時間</summary>
+    private float delta = 0.0f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // 時間計測
+        delta += Time.deltaTime;
+
+        // 奥に移動
+        transform.Translate(0, 0, speed);
+
+        // 計測時間が破棄時間に達した場合
+        if (delta > deletTime)
+        {
+            // 破棄
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// 衝突処理
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 破棄
+        Destroy(gameObject);
+    }
+}

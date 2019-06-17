5dff15daf18c46f95d402b4cda2c7a893b539414
Tatyumi <s150204@nishitech.ac.jp>
2019-06-17 22:54:34 +0900

    オーバーライドを用いてApplyDamageメソッドを追加
    ボスキャラは他のキャラと異なり、ダメージSE、死後の有り方が異なるため、オーバーライドを用いた。

--- Assets/Scripts/FirstBossController.cs
+++ Assets/Scripts/FirstBossController.cs
@@ -12,4 +12,35 @@ public class FirstBossController : EnemyController
         // 回転処理
         transform.Rotate(moveSpeed, moveSpeed, 0);
     }
+
+
+    /// <summary>
+    /// ダメージを適用する
+    /// </summary>
+    public override void ApplyDamage()
+    {
+        // 体力を1減らす 
+        hp -= 1;
+
+        // 体力が0以下の場合
+        if (hp <= 0)
+        {
+            // 破壊SE再生
+            audioManager.PlaySE(audioManager.DestroySE.name);
+
+            // スコア加算
+            ScoreController.AddScore(EnemyData.Score);
+
+            // オブジェクトを無効にする
+            enabled = false;
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            // 体力が残っている場合
+
+            // ダメージSEを再生
+            audioManager.PlaySE(audioManager.DamageSE.name);
+        }
+    }
 }

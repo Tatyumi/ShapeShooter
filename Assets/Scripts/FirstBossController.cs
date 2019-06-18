747bbc9a8773caab752345520a3e4a4c7871d2a3
Tatyumi <s150204@nishitech.ac.jp>
2019-06-18 15:43:23 +0900

    破壊演出を追加
    hpが0になった際に、パーティクルシステムを再生する機能の追加をした。

--- Assets/Scripts/FirstBossController.cs
+++ Assets/Scripts/FirstBossController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class FirstBossController : EnemyController
 {
+    /// <summary>破壊演出</summary>
+    public ParticleSystem DestroyDirection;
+    /// <summary>ジェネレーター</summary>
+    public EnemyBulletGenerator EnemyBulletGenerator;
+
     /// <summary>
     /// 移動処理
     /// </summary>
@@ -12,7 +17,6 @@ public class FirstBossController : EnemyController
         // 回転処理
         transform.Rotate(moveSpeed, moveSpeed, 0);
     }
-
 
     /// <summary>
     /// ダメージを適用する
@@ -25,11 +29,26 @@ public class FirstBossController : EnemyController
         // 体力が0以下の場合
         if (hp <= 0)
         {
+            // パーティクルシステムを再生
+            DestroyDirection.Play();
+
+            // ボスの位置にパーティクルシステムを配置
+            DestroyDirection.transform.localPosition = transform.localPosition;
+
             // 破壊SE再生
             audioManager.PlaySE(audioManager.DestroySE.name);
 
             // スコア加算
             ScoreController.AddScore(EnemyData.Score);
+
+            // nullチェック
+            if (EnemyBulletGenerator != null)
+            {
+                // nullでない場合
+
+                // ジェネレータを無効にする
+                EnemyBulletGenerator.GetComponent<EnemyBulletGenerator>().enabled = false;
+            }
 
             // オブジェクトを無効にする
             enabled = false;

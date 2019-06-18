ea691a3b1df73ea8388c6f4c44f729cbe7312394
Tatyumi <s150204@nishitech.ac.jp>
2019-06-19 00:25:47 +0900

    新規追加

---
+++ Assets/Scripts/BossStageDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossStageDirector : StageDirector
+{
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    protected override void Initialize()
+    {
+        // 音楽データの取得
+        audioManager = AudioManager.Instance;
+
+        // BGMの再生
+        audioManager.PlayBGM(StageBgmName);
+
+        // フラグ初期化
+        PauseManager.isPause = false;
+    }
+}

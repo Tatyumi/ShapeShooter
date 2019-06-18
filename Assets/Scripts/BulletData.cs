b92995cf493905e8dc91fc6cb82b7f75159dc053
Tatyumi <s150204@nishitech.ac.jp>
2019-06-19 00:08:37 +0900

    新規追加

---
+++ Assets/Scripts/BulletData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// アセットメニューに追加
+[CreateAssetMenu(menuName = "MyScriptable/Create BulletData")]
+
+
+public class BulletData : ScriptableObject
+{
+    /// <summary>弾速</summary>
+    public float Speed;
+    /// <summary>生存時間</summary>
+    public float LifeTime;
+}

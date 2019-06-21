dcd9dc9685bfc0d8e833dc26e9ed21ec04229d82
Tatyumi <s150204@nishitech.ac.jp>
2019-06-22 00:42:43 +0900

    新規追加

---
+++ Assets/Scripts/SecondBossBulletGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondBossBulletGenerator : EnemyBulletGenerator
+{
+    /// <summary>ボス</summary>
+    public GameObject Boss;
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    protected override void Initialize()
+    {
+        // オーディオマネージャーの取得
+        audioManager = AudioManager.Instance;
+
+        // 攻撃間隔の初期化
+        span = 0.7f;
+
+        // 無効にする
+        enabled = false;
+    }
+
+    /// <summary>
+    /// 生成する
+    /// </summary>
+    protected override void Generat()
+    {
+        // SEの再生
+        audioManager.PlaySE(audioManager.BossBulletSE.name);
+
+        // 生成オブジェクト格納配列
+        GameObject gameObject = Instantiate(BulletPrefab) as GameObject;
+
+        // 中ボスの座標に配置する
+        gameObject.transform.position = Boss.transform.position;
+
+        // プレイヤーの方向を向く
+        gameObject.transform.LookAt(Player.transform.position);
+    }
+}
---
+++ Assets/Scripts/SecondBossController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondBossController : EnemyController
+{
+    /// <summary>破壊演出</summary>
+    public ParticleSystem DestroyDirection;
+    /// <summary>ジェネレーター</summary>
+    public EnemyBulletGenerator EnemyBulletGenerator;
+    /// <summary>リザルトパネル</summary>
+    public GameObject ResultPanel;
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    protected override void Initialize()
+    {
+        // 音楽データの取得
+        audioManager = AudioManager.Instance;
+
+        // 体力の取得
+        hp = EnemyData.Hp;
+
+        // 移動速度の取得
+        moveSpeed = EnemyData.MoveSpeed;
+
+        // 無効にする
+        enabled = false;
+    }
+
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    protected override void Move()
+    {
+        // 回転処理
+        transform.Rotate(moveSpeed, moveSpeed, 0.0f);
+
+        // y方向に動く
+        transform.Translate(0, Mathf.Sin(Time.time * moveSpeed) * 0.2f, 0);
+    }
+
+    /// <summary>
+    /// ダメージを適用する
+    /// </summary>
+    public override bool ApplyDamage()
+    {
+        // 体力を1減らす 
+        hp -= 1;
+
+        // hpチェック
+        if (hp <= 0)
+        {
+            // hpが0以下の場合
+
+            // 音楽の停止
+            audioManager.StopSound();
+
+            // ボスの位置にパーティクルシステムを配置
+            DestroyDirection.transform.localPosition = transform.localPosition;
+
+            // パーティクルシステムを再生
+            DestroyDirection.Play();
+
+            // 破壊SE再生
+            audioManager.PlaySE(audioManager.DestroySE.name);
+
+            // スコア加算
+            ScoreController.AddScore(EnemyData.Score);
+
+            // nullチェック
+            if (EnemyBulletGenerator != null)
+            {
+                // nullでない場合
+
+                // ジェネレータを無効にする
+                EnemyBulletGenerator.GetComponent<EnemyBulletGenerator>().enabled = false;
+            }
+
+            // リザルトパネルを表示する
+            ResultPanel.SetActive(true);
+
+            // オブジェクトを無効にする
+            enabled = false;
+            gameObject.SetActive(false);
+
+            return true;
+        }
+        else
+        {
+            // hpが1以上の場合
+
+            // ダメージSEを再生
+            audioManager.PlaySE(audioManager.DamageSE.name);
+
+            return false;
+        }
+    }
+}

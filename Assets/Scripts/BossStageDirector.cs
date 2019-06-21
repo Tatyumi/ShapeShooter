12174126678a4af21d301aaa5d00aff515764435
Tatyumi <s150204@nishitech.ac.jp>
2019-06-21 22:43:24 +0900

    Boss切り替え機能実装
    クリアしたステージによって表示するBossの切り替え機能を実装した。

--- Assets/Scripts/BossStageDirector.cs
+++ Assets/Scripts/BossStageDirector.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Common;
 using UnityEngine;
+using System;
 
 public class BossStageDirector : StageDirector
 {
+    /// <summary>ボスオブジェクトリスト</summary>
+    public GameObject BossLists;
+    /// <summary>ボストランスフォームズ</summary>
+    private Transform bossTransforms;
+
     /// <summary>
     /// 初期化
     /// </summary>
@@ -17,5 +22,21 @@ public class BossStageDirector : StageDirector
 
         // フラグ初期化
         PauseManager.isPause = false;
+
+        // BossListsの子要素をすべて取得
+        bossTransforms = BossLists.GetComponentInChildren<Transform>();
+
+        // インスタンス取得
+        var sceneName = new SceneName();
+
+        // 挑戦したステージ名からステージ名リストの要素番号を取得する
+        var activeBossNumber= Array.IndexOf(sceneName.STAGE_NAMES, ChallengeStageName);
+
+        // bossTransformsの子要素の有効、無効の切り替えを行う
+        for (int i = 0; i < bossTransforms.childCount; i++)
+        {
+            // 該当するBossを有効にする
+            bossTransforms.GetChild(i).gameObject.SetActive(activeBossNumber == i);
+        }
     }
 }

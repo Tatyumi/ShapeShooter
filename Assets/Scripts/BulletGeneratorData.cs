fbed77a24770504ea719eb00382c9b320ec58450
Tatyumi <s150204@nishitech.ac.jp>
2019-06-24 13:25:07 +0900

    新規追加

---
+++ Assets/Scripts/BulletGeneratorData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// アセットメニューに追加
+[CreateAssetMenu(menuName = "MyScriptable/Create BulletGeneratorData")]
+
+public class BulletGeneratorData : ScriptableObject
+{
+    /// <summary>生成間隔</summary>
+    public float Span;
+}
---
+++ Assets/Scripts/ThirdBossBulletGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdBossBulletGenerator : EnemyBulletGenerator
+{
+    /// <summary>
+    /// 生成する
+    /// </summary>
+    protected override void Generat()
+    {
+        // SEの再生
+        audioManager.PlaySE(audioManager.BossBulletSE.name);
+
+        // 生成オブジェクト格納配列
+        GameObject gameObject = Instantiate(BulletPrefab) as GameObject;
+
+        // ゲームオブジェクトをPauseManagerの子にする
+        gameObject.transform.SetParent(PauseManager.transform, false);
+
+        gameObject.transform.position = this.transform.position;
+
+        // プレイヤーの方向を向く
+        gameObject.transform.LookAt(Player.transform.position);
+    }
+}

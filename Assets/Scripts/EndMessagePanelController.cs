19a3034b5a1fb6a40816fa3ced50ac267b2832e2
Tatyumi <s150204@nishitech.ac.jp>
2019-06-26 00:08:38 +0900

    新規追加

---
+++ Assets/Scripts/EndMessagePanelController.cs
@@ -0,0 +1,108 @@
+﻿using Common;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMessagePanelController : MonoBehaviour
+{
+    /// <summary>クリアメッセージ</summary>
+    public GameObject ClearMessage;
+    /// <summary>お礼のメッセージ</summary>
+    public GameObject ThankMessage;
+    /// <summary>グッドバイメッセージ</summary>
+    public GameObject GoodByeMessage;
+    /// <summary>オーディオマネージャー</summary>
+    private AudioManager audioManager;
+    /// <summary>リザルトフラグ</summary>
+    private bool isResult;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 初期化
+        Initialize();
+    }
+
+    private void Update()
+    {
+        // オブジェクトの活性状態とフラグチェック
+        if (gameObject.activeSelf && !isResult)
+        {
+            // オブジェクトが活性であり,かつフラグがfalseの場合
+
+            // フラグ更新
+            isResult = true;
+
+            // リザルトコルーチンを開始する
+            var corutine = StartResult();
+            StartCoroutine(corutine);
+        }
+    }
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    private void Initialize()
+    {
+        // 非表示
+        ClearMessage.SetActive(false);
+        ThankMessage.SetActive(false);
+        GoodByeMessage.SetActive(false);
+        gameObject.SetActive(false);
+
+        // 初期値代入
+        isResult = false;
+
+        // 音楽データの取得
+        audioManager = AudioManager.Instance;
+    }
+
+    /// <summary>
+    /// リザルトコルーチン
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator StartResult()
+    {
+        // 効果音の再生
+        audioManager.PlaySE(audioManager.ResultSE.name);
+
+        // ポーズ状態にして、操作負荷にする
+        PauseManager.isPause = true;
+
+        // 表示
+        gameObject.SetActive(true);
+
+        // 待機時間
+        var wait = new WaitForSeconds(1.5f);
+        // 次のシーンに遷移するまでの待機時間
+        var nextScenewait = new WaitForSeconds(7.5f);
+
+        // クリアメッセージの表示
+        ClearMessage.SetActive(true);
+        // 待機
+        yield return wait;
+
+        // 殺した敵キャラテキストの表示
+        ThankMessage.SetActive(true);
+        // テキスト表示SE再生
+        audioManager.PlaySE(audioManager.TextOnSE.name);
+        // 待機
+        yield return wait;
+
+        // テキスト表示SE再生
+        audioManager.PlaySE(audioManager.TextOnSE.name);
+        // 待機
+        yield return wait;
+
+        // ガイドテキストの表示
+        GoodByeMessage.SetActive(true);
+        // テキスト表示SE再生
+        audioManager.PlaySE(audioManager.TextOnSE.name);
+
+        // 次のシーンに遷移するまでの待機
+        yield return nextScenewait;
+
+        // タイトルシーンに遷移する
+        SceneManager.LoadScene(SceneName.TITLE_SCENE);
+    }
+}
---
+++ Assets/Scripts/TrueEndRollController.cs
@@ -0,0 +1,44 @@
+﻿using Common;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrueEndRollController : MonoBehaviour
+{
+    /// <summary>テキストのスクロールスピード</summary>
+    private float textScrollSpeed = 70.0f;
+    /// <summary>制限座標</summary>
+    private float limitPosition = 2782.0f;
+
+    void Start()
+    {
+        // フラグを更新する
+        LastBossController.isEnd = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // エンドロールが終了したか判別
+        if (!LastBossController.isEnd)
+        {
+            // 終了していない場合
+
+            // 制限座標を超えていないか判別
+            if (transform.localPosition.y <= limitPosition)
+            {
+                // 超えていない場合
+
+                // y座標に移動する
+                transform.position = new Vector2(transform.position.x, transform.position.y + textScrollSpeed * Time.deltaTime);
+            }
+            else
+            {
+                // 超えた場合
+
+                // フラグを更新する
+                LastBossController.isEnd = true;
+            }
+        }
+    }
+}
+

234913a67ae3443326f4f8a7ee5066a8aca3354d
Tatyumi <s150204@nishitech.ac.jp>
2019-06-26 00:14:29 +0900

    シーン遷移処理の修正
    隠しステージから遷移する場合は、ラスボスのシーンに遷移するように処理を修正した。

--- Assets/Scripts/WhiteLightImageController.cs
+++ Assets/Scripts/WhiteLightImageController.cs
@@ -9,7 +9,20 @@ public class WhiteLightImageController : FadeInUIController
         // 音の停止
         audioManager.StopSound();
 
-        // ボスシーンに遷移
-        SceneManager.LoadScene(SceneName.BOSS_SCENE);
+        // 現在のシーンが隠しステージか判別
+        if (SceneManager.GetActiveScene().name == SceneName.HIDDEN_STAGE_SCENE)
+        {
+            // 隠しステージの場合
+
+            // ラスボスシーンに遷移
+            SceneManager.LoadScene(SceneName.LAST_BOSS_SCENE);
+        }
+        else
+        {
+            // それ以外の場合
+
+            // ボスシーンに遷移
+            SceneManager.LoadScene(SceneName.BOSS_SCENE);
+        }
     }
 }

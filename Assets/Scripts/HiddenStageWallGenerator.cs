6b136a26f76c682fb7d31af700f27a09e2ef9a95
Tatyumi <s150204@nishitech.ac.jp>
2019-06-25 16:01:38 +0900

    新規追加

---
+++ Assets/Scripts/HiddenStageWallGenerator.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenStageWallGenerator : WallGenerator
+{
+    /// <summary>白光画像</summary>
+    public GameObject WhiteLightImage;
+
+    /// <summary>壁要素番号</summary>
+    private enum WallIndex
+    {
+        None = -1,
+        Vertical,
+        Horizon,
+        DiagonalLeft,
+        DiagonalRight,
+        BigVertical,
+        BigHorizon
+    }
+
+    /// <summary>
+    /// 壁生成コルーチン
+    /// </summary>
+    /// <returns></returns>
+    protected override IEnumerator StartGenerat()
+    {
+
+        // 次のフェイズまでの待機時間
+        var nextPhaseWait = new WaitForSeconds(3.5f);
+
+        // 待機時間
+        var wait = new WaitForSeconds(1.6f);
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 大縦壁
+        GeneratWall((int)WallIndex.BigVertical);
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 大横壁
+        GeneratWall((int)WallIndex.BigHorizon);
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        yield return wait;
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        yield return wait;
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+        yield return wait;
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        yield return wait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        yield return wait;
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        yield return wait;
+
+        // 大縦壁
+        GeneratWall((int)WallIndex.BigVertical);
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        yield return wait;
+
+        // 大横壁
+        GeneratWall((int)WallIndex.BigHorizon);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        yield return wait;
+
+        // 待機
+        yield return nextPhaseWait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+        yield return wait;
+
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        yield return wait;
+
+        // 大横壁
+        GeneratWall((int)WallIndex.BigHorizon);
+        yield return wait;
+
+        // 大縦壁
+        GeneratWall((int)WallIndex.BigVertical);
+        yield return wait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        yield return wait;
+
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        yield return wait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        yield return wait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        yield return wait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        yield return wait;
+
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        yield return wait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        // 横壁
+        GeneratWall((int)WallIndex.Horizon);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        yield return wait;
+
+        // 縦壁
+        GeneratWall((int)WallIndex.Vertical);
+        // 左斜め壁
+        GeneratWall((int)WallIndex.DiagonalLeft);
+        // 右斜め壁
+        GeneratWall((int)WallIndex.DiagonalRight);
+        yield return wait;
+
+        // 画像の表示
+        WhiteLightImage.SetActive(true);
+
+        // 終了
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/WallGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WallGenerator : MonoBehaviour
+{
+    /// <summary>ウォールプレファブリスト</summary>
+    public GameObject[] WallPrefabLists;
+    /// <summary>ポーズマネージャー</summary>
+    public GameObject PauseManager;
+    /// <summary>コルーチン</summary>
+    protected IEnumerator corutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コルーチンの取得
+        corutine = StartGenerat();
+
+        // 処理開始
+        StartCoroutine(corutine);
+    }
+
+    /// <summary>
+    /// 指定した壁を生成する
+    /// </summary>
+    /// <param name="index">要素番号</param>
+    protected void GeneratWall(int index)
+    {
+        //生成するプレファブをゲームオブジェクトに変換
+        var wallObj = Instantiate(WallPrefabLists[index]) as GameObject;
+
+        //ゲームオブジェクトをPauseManagerの子にする
+        wallObj.transform.SetParent(PauseManager.transform, false);
+
+        // ゲームオブジェクトを指定の座標に配置
+        wallObj.transform.position = transform.position;
+    }
+
+    /// <summary>
+    /// コルーチン停止
+    /// </summary>
+    public void Stop()
+    {
+        StopCoroutine(corutine);
+    }
+
+    /// <summary>
+    /// コルーチン再開
+    /// </summary>
+    public void Restart()
+    {
+        StartCoroutine(corutine);
+    }
+
+    // 生成開始処理
+    protected abstract IEnumerator StartGenerat();
+}

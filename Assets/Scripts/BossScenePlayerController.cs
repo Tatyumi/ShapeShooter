ffabb6d9c915e2d977bc6cfadedeca24b5fc6a8f
Tatyumi <s150204@nishitech.ac.jp>
2019-06-27 00:08:44 +0900

    不要な処理の削除
    初期化処理でオブジェクトの座標を変数に代入し、その値を用いていたが、オブジェクトから座標を取得することができるため、修正した。

--- Assets/Scripts/BossScenePlayerController.cs
+++ Assets/Scripts/BossScenePlayerController.cs
@@ -1,22 +1,7 @@
 ﻿using UnityEngine;
 
-public sealed class BossScenePlayerController : PlayerController
+public sealed class BossScenePlayerController : PlayerListsController
 {
-    /// <summary>対象物のポジション</summary>
-    private Vector3 targetPosition;
-
-    /// <summary>
-    /// 初期化
-    /// </summary>
-    protected override void Initialize()
-    {
-        // 初期化
-        base.Initialize();
-
-        // 対象のオブジェクトの座標を取得
-        targetPosition = TargetObj.transform.position;
-    }
-
     /// <summary>
     /// 移動処理
     /// </summary>
@@ -26,14 +11,14 @@ public sealed class BossScenePlayerController : PlayerController
         if (Input.GetKey(KeyCode.RightArrow))
         {
             // 対象オブジェクトの座標を中心に右に回転
-            transform.RotateAround(targetPosition, Vector3.down, MoveSpeed);
+            transform.RotateAround(TargetObj.transform.position, Vector3.down, MoveSpeed);
         }
 
         // ←を押した場合
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             // 対象オブジェクトの座標を中心に左に回転
-            transform.RotateAround(targetPosition, Vector3.up, MoveSpeed);
+            transform.RotateAround(TargetObj.transform.position, Vector3.up, MoveSpeed);
         }
     }
 }

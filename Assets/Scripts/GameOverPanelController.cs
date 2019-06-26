d4e4c265e99d1e4be5c14d94212352452f11ec12
Tatyumi <s150204@nishitech.ac.jp>
2019-06-26 13:28:54 +0900

    sealedクラスに変更
    継承しない想定であるため、sealedクラスに変更した。

--- Assets/Scripts/GameOverPanelController.cs
+++ Assets/Scripts/GameOverPanelController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine.SceneManagement;
 using Common;
 
-public class GameOverPanelController : FadeInUIController
+public sealed class GameOverPanelController : FadeInUIController
 {
     /// <summary>シーン遷移時の演出</summary>
     protected override void SceneMoveAction()

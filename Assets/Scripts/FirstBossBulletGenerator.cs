cfc46822badff3460d554ea1886f0acd8b6b7320
Tatyumi <s150204@nishitech.ac.jp>
2019-06-26 12:00:49 +0900

    sealedクラスに変更
    継承しない想定であるため、sealedクラスに変更した。

--- Assets/Scripts/FirstBossBulletGenerator.cs
+++ Assets/Scripts/FirstBossBulletGenerator.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class FirstBossBulletGenerator : EnemyBulletGenerator
+public sealed class FirstBossBulletGenerator : EnemyBulletGenerator
 {
     /// <summary>ボス</summary>
     public GameObject Boss;

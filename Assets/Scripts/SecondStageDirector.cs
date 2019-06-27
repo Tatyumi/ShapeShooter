933bcdc52555a62e7187d5f9630076002c025575
Tatyumi <s150204@nishitech.ac.jp>
2019-06-27 16:26:20 +0900

    新規追加

---
+++ Assets/Scripts/SecondStageDirector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SecondStageDirector : StageDirector
+{
+    /// <summary>アドバイステキスト</summary>
+    public GameObject AdviceText;
+    /// <summary>プレイしたかチェックフラグ</summary>
+    private static bool isSecondStagePlay = true;
+
+
+    private void Start()
+    {
+        // 初回プレイか判別
+        if (isSecondStagePlay)
+        {
+            // 初回プレイの場合
+
+            // アドバイス表示コルーチンを起動
+            var corutine = DisplayAdvice();
+            StartCoroutine(corutine);
+        }
+        else
+        {
+            // 初回プレイ出ない場合
+
+            // 非表示
+            AdviceText.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// アドバイス表示コルーチン
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator DisplayAdvice()
+    {
+        // 待機時間
+        var wait = new WaitForSeconds(3);
+
+        // 表示
+        AdviceText.SetActive(true);
+
+        // 待機
+        yield return wait;
+
+        // 非表示
+        AdviceText.SetActive(false);
+
+        // フラグ更新
+        isSecondStagePlay = false;
+    }
+}
